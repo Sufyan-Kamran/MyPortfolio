c5325bc995f63791ba9136e7567980ad42831b86
Sufyan-Kamran <sufyankamran171@gmail.com>
2025-11-19 10:01:31 +0500

    Add GitHub Pages workflow

--- src/App.js
+++ src/App.js
@@ -7,14 +7,12 @@ import PortfolioPage from "./pages/PortfolioPage.jsx";
 
 function App(){
   return (
-    <BrowserRouter>
+    <BrowserRouter  basename="/MyPortfolio">
     <Navbar />
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/About" element={<AboutPage/>}/>
         <Route path="/Portfolio" element={<PortfolioPage />}/>
-        
-
       </Routes>
     </BrowserRouter>
   )
